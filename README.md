3194fcf55686e50c2dbe07b280a119b384963da0
DivyanshS26 <divyanshsahu25@gmail.com>
2025-08-16 16:03:43 +0200

    Step - 1 Removing indentation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Code-Kata
+
+**Step 1**
+Each method should have just one block of indentation:
+
+1. No nested if/else, loops, or switch inside a method.
+
+2. If you need more nesting, extract to a new method.
--- src/main/java/com/cgi/tennis/Step1.java
+++ src/main/java/com/cgi/tennis/Step1.java
@@ -1,9 +1,11 @@
 package com.cgi.tennis;
 
+import java.util.Objects;
+
 public class Step1 implements TennisGame {
-    
-    private int p2;
+
     private int p1;
+    private int p2;
     private String p1N;
     private String p2N;
 
@@ -13,25 +15,50 @@ public class Step1 implements TennisGame {
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+        if (isNormalScore()) {
+            return getNormalScore();
         }
-    }
-    
-    public void wonPoint(String playerName) {
-        if (playerName == this.p1N)
-            this.p1 += 1;
-        else
-            this.p2 += 1;
-        
+        return getSpecialScore();
     }
 
+    private boolean isNormalScore() {
+        return p1 < 4 && p2 < 4 && (p1 + p2 != 6);
+    }
+
+    private String getNormalScore() {
+        String[] points = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        String score = points[p1];
+        if (p1 == p2) {
+            return score + "-All";
+        }
+        return score + "-" + points[p2];
+    }
+
+    private String getSpecialScore() {
+        if (p1 == p2) {
+            return "Deuce";
+        }
+        String player = p1 > p2 ? p1N : p2N;
+        if (isAdvantageScore()) {
+            return "Advantage " + player;
+        }
+        return "Win for " + player;
+    }
+
+    private boolean isAdvantageScore() {
+        return squareOfDifference() == 1;
+    }
+
+    private int squareOfDifference() {
+        int diff = p1 - p2;
+        return diff * diff;
+    }
+
+    public void wonPoint(String playerName) {
+        if (Objects.equals(playerName, this.p1N)) {
+            this.p1 += 1;
+        } else {
+            this.p2 += 1;
+        }
+    }
 }
